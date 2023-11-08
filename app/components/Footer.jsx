f0d77c24308c07cd90e6efaf010b17c0dd928c64
Ric Taglietti <rictaglietti@gmail.com>
2023-11-08 17:42:20 -0500

    center footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from '../../public/images/cloud-logo.png';
 const Footer = () => {
 	return (
 		<footer className="footer border border-t-[#33353F] z-10 border-l-transparent border-r-transparent text-white">
-			<div className="flex container mx-auto justify-between">
+			<div className="flex container mx-auto justify-center">
 				<Image src={Logo} height={100} alt="logo" />
 			</div>
 		</footer>
