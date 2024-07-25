28e2de05e0d06564ce93dba9441005deca102f8f
Ric Taglietti <rictaglietti@gmail.com>
2024-07-25 13:34:34 -0400

    simplify layout

--- app/page.js
+++ app/page.js
@@ -1,20 +1,14 @@
 import Image from 'next/image';
 import HeroSection from './components/HeroSection';
-import Navbar from './components/Navbar';
 import AboutSection from './components/AboutSection';
-import EmailSection from './components/EmailSection';
-import Footer from './components/Footer';
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col bg-[#121212]">
-			<Navbar />
 			<div className="container mt-24 mx-auto px-12 py-4">
 				<HeroSection />
 				<AboutSection />
-				<EmailSection />
 			</div>
-			<Footer />
 		</main>
 	);
 }
